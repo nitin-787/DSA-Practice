bac4189cee84e948e06723ce9e43304baad320aa
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-01-08 18:55:02 +0530

    assignment solution - 05-Array - Concatenation of array

---
+++ assignment-solutions/src/Arrays/Concatenation.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Arrays;
+
+class Concatenation {
+    public static void main(String[] args) {
+        int[] nums = {};
+        System.out.println(Arrays.toString(getConcatenation(nums)));
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+        int i = 0, length = nums.length;
+        int[] ans = new int[length * 2];
+
+        while (length > i) {
+            ans[i] = nums[i];
+            ans[length + i] = nums[i];
+            i++;
+        }
+        return ans;
+    }
+}
