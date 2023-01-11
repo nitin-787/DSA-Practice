7a78d3ec546915ea6d966d665590bf4a8f147f31
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-01-11 16:19:33 +0530

    assignment solution - 05-Array - Kids With the Greatest Number of Candies

---
+++ assignment-solutions/src/Arrays/KidsWithCandies.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+// Ques : 1431. Kids With the Greatest Number of Candies
+// Link : https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+// Detailed Solution : https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/solutions/3035040/easy-to-understand-java-solution/
+
+import java.util.*;
+
+public class KidsWithCandies {
+    public static void main(String[] args) {
+        int[]candies = {4,2,1,1,2};
+        int extraCandies = 1;
+        System.out.println(kidsWithCandies(candies, extraCandies));
+    }
+
+    static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        // finding the largest element first
+        int start = 0, end = candies.length, largest = 0;
+        while (start < end) {
+            largest = Math.max(largest, candies[start]);
+            start++;
+        }
+        System.out.println(largest);
+
+        List<Boolean> list = new ArrayList<Boolean>();
+        // reset the value of start
+        start = 0;
+        while (start < end) {
+            if (candies[start] + extraCandies >= largest) {
+                list.add(true);
+            } else {
+                list.add(false);
+            }
+            start++;
+        }
+        return list;
+    }
+}
