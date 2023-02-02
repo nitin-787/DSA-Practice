1a15044412b83d251675a25b8f125e91faa60768
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-02-02 22:21:45 +0530

    assignment solution - 06-sorting - 441. Arranging Coins

---
+++ assignment-solutions/src/Searching/Solution.java
@@ -0,0 +1,28 @@
+package Searching;
+
+// Ques : 441. Arranging Coins
+// link : https://leetcode.com/problems/arranging-coins/
+class Solution {
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println(arrangeCoins(n));
+    }
+    static int arrangeCoins(int n) {
+        long start = 1, end = n;
+
+        while(start <= end){
+            long mid = start + (end - start)/2;
+            long sum  = mid * (mid + 1) / 2;
+
+            if(sum == n){
+                return (int)mid;
+            } else if(sum < n){
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+
+        return (int)end;
+    }
+}
