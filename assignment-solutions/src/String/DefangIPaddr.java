ea4f6b10392218fe19869ce5753480e0cc020735
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-03-18 22:22:40 +0530

    assignment solution - 08-String - 1108. Defanging an IP Address

---
+++ assignment-solutions/src/String/DefangIPaddr.java
@@ -0,0 +1,28 @@
+package String;
+
+// Ques: 1108. Defanging an IP Address
+// link:  https://leetcode.com/problems/defanging-an-ip-address/
+class DefangIPaddr {
+    public static void main(String[] args) {
+        String address = "1.1.1.1";
+        System.out.println(defangIPaddr(address));
+    }
+    public static String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        int start = 0;
+        int end = 0;
+        while (end < address.length()) {
+            if (address.charAt(end) == '.') {
+                sb.append(address, start, end);
+                sb.append("[.]");
+                start = end + 1;
+            }
+            end++;
+        }
+        sb.append(address, start, end);
+        return sb.toString();
+
+        // using in-built function
+        // return address.replaceAll("\\.", "[.]");
+    }
+}
