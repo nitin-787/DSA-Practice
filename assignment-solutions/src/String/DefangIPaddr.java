bb99ed0a503bad5ceca07faf6496005a16d5c9ca
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-03-21 23:16:07 +0530

    assignment solution - 08-String - 1528. Shuffle String

--- assignment-solutions/src/String/DefangIPaddr.java
+++ assignment-solutions/src/String/DefangIPaddr.java
@@ -2,15 +2,14 @@ package String;
 
 // Ques: 1108. Defanging an IP Address
 // link:  https://leetcode.com/problems/defanging-an-ip-address/
-class DefangIPaddr {
+public class DefangIPaddr {
     public static void main(String[] args) {
         String address = "1.1.1.1";
         System.out.println(defangIPaddr(address));
     }
     public static String defangIPaddr(String address) {
         StringBuilder sb = new StringBuilder();
-        int start = 0;
-        int end = 0;
+        int start = 0, end = 0;
         while (end < address.length()) {
             if (address.charAt(end) == '.') {
                 sb.append(address, start, end);
---
+++ assignment-solutions/src/String/RestoreString.java
@@ -0,0 +1,19 @@
+package String;
+
+// ques : 1528. Shuffle String
+// link : https://leetcode.com/problems/shuffle-string/
+
+class RestoreString {
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = {4,5,6,7,0,2,1,3};
+        System.out.println(restoreString(s, indices));
+    }
+    public static String restoreString(String s, int[] indices) {
+        char[] result = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            result[indices[i]] = s.charAt(i);
+        }
+        return new String(result);
+    }
+}
