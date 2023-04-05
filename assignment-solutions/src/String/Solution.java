b747d9f8f5384a05a68b03e17dd761910ce31492
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-04-05 12:44:49 +0530

    assignment solution - 08-String - 709. To Lower Case (without using inbuilt function)

---
+++ assignment-solutions/src/String/Solution.java
@@ -0,0 +1,25 @@
+package String;
+
+// Ques: 709. To Lower Case (without using inbuilt function)
+// link: https://leetcode.com/problems/to-lower-case/
+class Solution {
+    public static void main(String[] args) {
+        String s = "HELLO";
+        System.out.println(toLowerCase(s));
+    }
+
+    public static String toLowerCase(String s) {
+        StringBuilder str = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                str.append((char)(c + ('a' - 'A')));
+            } else {
+                str.append(c);
+            }
+        }
+        return str.toString();
+
+        // using in-built function
+        // return s.toLowerCase();
+    }
+}
