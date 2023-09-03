dd533b8777623f3b3692c3ff2c381a582839f196
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-09-03 23:27:16 +0530

    leetcode interview question day4

---
+++ assignment-solutions/src/InterviewQues/Easy/ValidParentheses.java
@@ -0,0 +1,25 @@
+package InterviewQues.Easy;
+
+import java.util.Stack;
+
+class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else { 
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.peek();
+                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+                    stack.pop();
+                } else { 
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
