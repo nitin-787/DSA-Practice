07aa19121ab3376dfcb37fd73efdd0b72689829e
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-09-22 23:33:16 +0530

    leetcode interview question no 9

--- assignment-solutions/src/InterviewQues/Easy/MaxProfit.java
+++ assignment-solutions/src/InterviewQues/Easy/MaxProfit.java
@@ -1,36 +1,34 @@
 package InterviewQues.Easy;
 
+// link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
 public class MaxProfit {
     public static void main(String[] args) {
-        int[] arr = {7,2,5,3,6,4};
+        int[] arr = {7, 1, 5, 3, 6, 4};
         System.out.println(maxProfit(arr));
     }
 
     static int maxProfit(int[] arr) {
-        int profit = 0, smallest = arr[0], largest = 0, n = arr.length-1, j = 0, k = 0;
-        // find the smallest number first in the array
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < smallest) {
-                smallest = arr[i];
-                j = i;
-            }
-        }
-        if (smallest == arr[n]) {
-            return profit;
-        } else {
-            // find the largest number it must be after the smallest, if not the return 0
-            for (int i = arr[smallest]; i < arr.length; i++) {
-                if (arr[i] > largest) {
-                    largest = arr[i];
-                    k = i;
-                }
-                System.out.println(largest);
-            }
-        }
-        if (j < k) {
+        if (arr.length <= 1) {
             return 0;
         }
-        // largest number - smallest
-        return largest-smallest;
+
+        int minPrice = arr[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            int currentPrice = arr[i];
+            int potentialProfit = currentPrice - minPrice;
+
+            if (potentialProfit > maxProfit) {
+                maxProfit = potentialProfit;
+            }
+
+            if (currentPrice < minPrice) {
+                minPrice = currentPrice;
+            }
+        }
+
+        return maxProfit;
     }
 }
