a8e362d71c6242cbd211a58e8ed735d95fba6a16
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-10-14 22:27:37 +0530

    optimized solution: First and last oucurrence in an array

--- assignment-solutions/src/Arrays/FirstAndLastPosition.java
+++ assignment-solutions/src/Arrays/FirstAndLastPosition.java
@@ -2,11 +2,48 @@ package Arrays;
 
 import java.util.Arrays;
 
+//Link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 public class FirstAndLastPosition {
     public static void main(String[] args) {
         int[] arr = {5,5,7,7,8,8,10};
         int target = 5;
         System.out.println(Arrays.toString(firstAndLast(arr, target)));
+        System.out.println(Arrays.toString(firstAndLast1(arr, target)));
+    }
+
+    // Modified binary search, time complexity O(log n)
+    static int[] firstAndLast1(int[] arr, int target) {
+    int firstOccurrence = binarySearch(arr, target, true);
+    int lastOccurrence = binarySearch(arr, target, false);
+
+        if (firstOccurrence == -1) {
+        return new int[]{-1, -1};
+    }
+
+        return new int[]{firstOccurrence, lastOccurrence};
+}
+
+    static int binarySearch(int[] arr, int target, boolean findFirst) {
+        int index = -1, start = 0, end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                index = mid;
+                if (findFirst) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return index;
     }
 
     static int[] firstAndLast(int[] arr, int target) {
--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@
 |  isPalindrome| https://leetcode.com/problems/valid-palindrome/  |  two-pointer | optimized |  |
 | Best time to buy and sell the stock | https://leetcode.com/problems/best-time-to-buy-and-sell-stock/ |  |  | https://github.com/nitin-787/Java-DSA-Practice/blob/07aa19121ab3376dfcb37fd73efdd0b72689829e/assignment-solutions/src/InterviewQues/Easy/MaxProfit.java |
 |  Largest number | https://leetcode.com/problems/largest-number/  |  |  Optimized | https://github.com/nitin-787/Java-DSA-Practice/blob/main/assignment-solutions/src/InterviewQues/Medium/LargestNumber.java  |
-|  |  |  |  |  |
+|  First and Last Ouccrence | https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/ | Binary Search | Optimized | https://github.com/nitin-787/Java-DSA-Practice/blob/9d2692b69376d5a0f7aa4fa27e7a228c415eea00/assignment-solutions/src/Arrays/FirstAndLastPosition.java  |
 |  |  |  |  |  |
 |  |  |  |  |  |
 |  |  |  |  |  |
