59bd0fa90d1697c599f16914cb96d29917cd4f8d
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-11-10 21:46:56 +0530

    Special Array With X Elements Greater Than or Equal X - optimized

--- assignment-solutions/src/Searching/GreaterThanEqualX.java
+++ assignment-solutions/src/Searching/GreaterThanEqualX.java
@@ -2,10 +2,13 @@ package Searching;
 
 // link: https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
 
+import java.util.Arrays;
+
 public class GreaterThanEqualX {
     public static void main(String[] args) {
-        int[] arr = {0,4,3,0,4};
+        int[] arr = {3,5};
         System.out.println(greaterThanEqualX(arr));
+        System.out.println(greaterThanEqualX1(arr));
     }
 
     static int greaterThanEqualX(int[] arr) {
@@ -19,6 +22,30 @@ public class GreaterThanEqualX {
             if (count == i) {
                 return i;
             }
+        }
+        return -1;
+    }
+
+    // Sorting + Binary Search: time complexity = O(n log n)
+    static int greaterThanEqualX1(int[] arr) {
+        Arrays.sort(arr);
+        int i = arr.length;
+
+        while(i>0){
+            int start=0, end = arr.length-1, count=0;
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                if(arr[mid] >= i){
+                    count += end-mid+1;
+                    end = mid-1;
+                } else {
+                    start = mid+1;
+                }
+            }
+            if(count==i){
+                return i;
+            }
+            i--;
         }
         return -1;
     }
